15d010b99ba194e7a812f826620574600cbce3ec
TingjiaInFuture <109398380+TingjiaInFuture@users.noreply.github.com>
2024-01-17 15:10:59 +0800

    Update server.js

--- server.js
+++ server.js
@@ -86,10 +86,11 @@ const authorized = key => {
 };
 
 // Run the server and report out to the logs
-fastify.listen({port:process.env.PORT, host:'0.0.0.0'}, function(err, address) {
+fastify.listen({port:9000, host:'0.0.0.0'}, function(err, address) {
   if (err) {
     console.error(err);
     process.exit(1);
   }
   console.log(`Your app is listening on ${address}`);
 });
+
